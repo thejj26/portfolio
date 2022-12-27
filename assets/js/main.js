fd6b8a982cbb9007aa425319d2f373746368e0ce
thejj26 <65808980+thejj26@users.noreply.github.com>
2022-12-27 22:32:06 +0100

    mobile menu, footer
    - added a working mobile menu (no animations yet)
    - REMOVED the active section border alongside its js functions and everything similar
    - added a simple footer

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,22 @@
-let navSections=document.getElementsByClassName("nav-section")  //stores all of the navigation links into this element
+let fullscreen = document.getElementById("mobile-menu-fullscreen")
+let sections = document.getElementsByTagName("section")
 
-function enableNavBorder(navSection){
-   for(let i=0;i<navSections.length;i++){
-        navSections[i].parentElement.setAttribute("inView","false") //sets the inView attribute of all element to false (clear all of them)
-   }
-   navSection.parentElement.setAttribute("inView","true")   //sets the inView attribute of the selected element to true
-}
-
-document.addEventListener("DOMContentLoaded",()=>{ //when the site loads execute the event
-   document.getElementById("home").style.marginTop="0";    //remove the margin
-   document.getElementById("home").style.opacity="1"; //set the opacity to 1 (fully visible)
+document.addEventListener("DOMContentLoaded", () => { //when the site loads execute the event
+   document.getElementById("home").style.marginTop = "0";    //remove the margin
+   document.getElementById("home").style.opacity = "1"; //set the opacity to 1 (fully visible)
 })
 
+
+function showFullscreen() {
+   fullscreen.style.display = "flex"
+   for (let i = 0; i < sections.length; i++) {
+      sections[i].style.display = "none"
+   }
+}
+
+function hideFullscreen() {
+   fullscreen.style.display = "none"
+   for (let i = 0; i < sections.length; i++) {
+      sections[i].style.display = ""
+   }
+}
