bf4d9d973db0b077e22f997c2671cef342088578
thejj26 <65808980+thejj26@users.noreply.github.com>
2022-12-28 16:08:46 +0100

    image scroll and other changes
    - changes to the grid system (now every element has a 100% height)
    - added a new fullscreen element for selecting images
    - a new system for selecting images
    - new functions for controlling scrolling
    - minor fullscreen changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,8 @@
-let fullscreen = document.getElementById("mobile-menu-fullscreen")
-let sections = document.getElementsByTagName("section")
+let fullscreenMenu = document.getElementById("mobile-menu-fullscreen")
+let body = document.getElementsByTagName("body")[0]
+
+let portraitImages = ["https://thispersondoesnotexist.com/image", "https://thispersondoesnotexist.com/image", "https://thispersondoesnotexist.com/image", "https://thispersondoesnotexist.com/image", "https://thispersondoesnotexist.com/image"]
+let activePortrait = 0;
 
 document.addEventListener("DOMContentLoaded", () => { //when the site loads execute the event
    document.getElementById("home").style.marginTop = "0";    //remove the margin
@@ -7,16 +10,53 @@ document.addEventListener("DOMContentLoaded", () => { //when the site loads exec
 })
 
 
-function showFullscreen() {
-   fullscreen.style.display = "flex"
-   for (let i = 0; i < sections.length; i++) {
-      sections[i].style.display = "none"
-   }
+function disableScrollInFulscreen() {
+   body.classList.add("no-scroll")
 }
 
-function hideFullscreen() {
-   fullscreen.style.display = "none"
-   for (let i = 0; i < sections.length; i++) {
-      sections[i].style.display = ""
+function enableScrollInFullscreen() {
+   body.classList.remove("no-scroll")
+}
+
+function showFullscreenMenu() {
+   fullscreenMenu.style.display = "flex"
+   disableScrollInFulscreen()
+}
+
+function hideFullscreenMenu() {
+   fullscreenMenu.style.display = "none"
+   enableScrollInFullscreen()
+}
+
+function showFullscreenImg(divId) {
+   disableScrollInFulscreen()
+   document.getElementById(divId).style.display = "flex"
+}
+
+function hideFullscreenImg(divId) {
+   document.getElementById(divId).style.display = "none"
+   enableScrollInFullscreen()
+}
+
+function chImg(imgId, mode, index) {
+   document.getElementById(imgId + activePortrait).classList.remove("fill")
+   switch (mode) {
+      case 0:
+         if (activePortrait>0) {
+            activePortrait--
+            document.getElementById(imgId).setAttribute("href", portraitImages[activePortrait])
+         }
+         break
+      case 1:
+         if (activePortrait < portraitImages.length-1) {
+            activePortrait++
+            document.getElementById(imgId).setAttribute("href", portraitImages[activePortrait])
+         }
+         break
+      case 2:
+         activePortrait = index
+         document.getElementById(imgId).setAttribute("href", portraitImages[activePortrait])
+         break
    }
+   document.getElementById(imgId + activePortrait).classList.add("fill")
 }
