f93dea890f497bae33826abe7d2fbab4db23e64b
thejj26 <65808980+thejj26@users.noreply.github.com>
2022-12-29 14:29:40 +0100

    updated about me
    - updated about me formatting
    - added text to the first content
    - added an image and image navigation to the second content

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,11 +2,15 @@ let fullscreenMenu = document.getElementById("mobile-menu-fullscreen")
 let body = document.getElementsByTagName("body")[0]
 
 let portraitImages = ["https://thispersondoesnotexist.com/image", "https://thispersondoesnotexist.com/image", "https://thispersondoesnotexist.com/image", "https://thispersondoesnotexist.com/image", "https://thispersondoesnotexist.com/image"]
-let activePortrait = 0;
+let activePortrait = 0
+
+let hobbyImages = ["https://thisartworkdoesnotexist.com/", "https://thisartworkdoesnotexist.com/", "https://thisartworkdoesnotexist.com/"]
+let activeHobby = 0
 
 document.addEventListener("DOMContentLoaded", () => { //when the site loads execute the event
    document.getElementById("home").style.marginTop = "0";    //remove the margin
    document.getElementById("home").style.opacity = "1"; //set the opacity to 1 (fully visible)
+   document.getElementById("about-me").style.opacity = "1";
 })
 
 
@@ -39,24 +43,48 @@ function hideFullscreenImg(divId) {
 }
 
 function chImg(imgId, mode, index) {
-   document.getElementById(imgId + activePortrait).classList.remove("fill")
-   switch (mode) {
-      case 0:
-         if (activePortrait>0) {
-            activePortrait--
+   if (imgId == "portrait-img") {
+      document.getElementById(imgId + activePortrait).classList.remove("fill")
+      switch (mode) {
+         case 0:
+            if (activePortrait > 0) {
+               activePortrait--
+               document.getElementById(imgId).setAttribute("href", portraitImages[activePortrait])
+            }
+            break
+         case 1:
+            if (activePortrait < portraitImages.length - 1) {
+               activePortrait++
+               document.getElementById(imgId).setAttribute("href", portraitImages[activePortrait])
+            }
+            break
+         case 2:
+            activePortrait = index
             document.getElementById(imgId).setAttribute("href", portraitImages[activePortrait])
-         }
-         break
-      case 1:
-         if (activePortrait < portraitImages.length-1) {
-            activePortrait++
-            document.getElementById(imgId).setAttribute("href", portraitImages[activePortrait])
-         }
-         break
-      case 2:
-         activePortrait = index
-         document.getElementById(imgId).setAttribute("href", portraitImages[activePortrait])
-         break
+            break
+      }
+      document.getElementById(imgId + activePortrait).classList.add("fill")
    }
-   document.getElementById(imgId + activePortrait).classList.add("fill")
+   else if (imgId == "hobbies-img") {
+      document.getElementById(imgId + activeHobby).classList.remove("fill")
+      switch (mode) {
+         case 0:
+            if (activeHobby > 0) {
+               activeHobby--
+               document.getElementById(imgId).setAttribute("href", hobbyImages[activeHobby])
+            }
+            break
+         case 1:
+            if (activeHobby < hobbyImages.length - 1) {
+               activeHobby++
+               document.getElementById(imgId).setAttribute("href", hobbyImages[activeHobby])
+            }
+            break
+         case 2:
+            activeHobby = index
+            document.getElementById(imgId).setAttribute("href", hobbyImages[activeHobby])
+            break
+      }
+      document.getElementById(imgId + activeHobby).classList.add("fill")
+   }
 }
